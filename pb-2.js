213f4eff08b0f5f649a3055b733320e59bda2844
nafiul-alam-29 <shishirght29@gmail.com>
2022-09-22 07:11:23 +0600

    problem 2 solve

---
+++ pb-2.js
@@ -0,0 +1,28 @@
+// Write a Program to Find you and your's friend grade using if-else
+
+var score = 76;
+
+if(score >= 80)
+{
+    console.log("Bhai/Apu Tmi A Grade Paiso");
+}
+
+else if(score >= 60)
+{
+    console.log("Bhai/Apu Tmi B Grade Paiso");
+}
+
+else if(score >= 50)
+{
+    console.log("Bhai/Apu Tmi C Grade Paiso");
+}
+
+else if(score >= 40)
+{
+    console.log("Bhai/Apu Tmi D Grade Paiso");
+}
+
+else{
+    console.log("Bhai/Apu tmi ki korso eida tmi fall kore felso");
+}
+
