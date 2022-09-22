896ad188496f4eb1d1eefb6f3fa271ada3ace5d7
nafiul-alam-29 <shishirght29@gmail.com>
2022-09-22 07:25:02 +0600

    problem 3 solve

---
+++ pb-3.js
@@ -0,0 +1,17 @@
+// write a program find out large number among give three value.
+
+var a= 1312;
+var b= 790;
+var c= 505;
+
+if(a >= b && a >= c){
+    console.log( a, " A is the large number" );
+}
+
+else if (b>=a && b>=c){
+    console.log(b, " B is the large number")
+}
+
+else{
+    console.log(c," C is the large number");
+}
