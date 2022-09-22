1b22b2cc67002ff680b541de58bf479d565b8729
nafiul-alam-29 <shishirght29@gmail.com>
2022-09-22 07:32:32 +0600

    problem 4 are solve

---
+++ pb-4.js
@@ -0,0 +1,16 @@
+var a= 9;
+var b= 12;
+var c= 12;
+
+if(a==b || a==c){
+    console.log(' A & C Two are equal value');
+}
+
+else if(b==a || b==c)
+{
+    console.log(' B & C Two are equal value');
+}
+
+else{
+    console.log("none of them are equal")
+}
